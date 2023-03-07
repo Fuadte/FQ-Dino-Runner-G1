3c7082a80b4f9298b9dc03cab125ebb73494b2ce
Joel Fuad Lopez <fuad.quispe@fundacion-jala.org>
2023-03-06 21:38:18 -0400

    Tarea 2: Large_cactus
    añadir el otro tipo de cactus

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -13,7 +13,7 @@ class Obstacle_manager:
         for obstacle in self.obstacles:
             obstacle.update(game_speed, self.obstacles)
             if player.rect.colliderect(obstacle.rect):
-                pygame.time.delay(2000)
+                pygame.time.delay(500)
                 game.playing = False
 
     def draw(self, screen):
